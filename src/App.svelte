ce311048ebaa960e2a63357b44d06e3ffa51882a
IDSCAN\bmulhall <bmulhall@idscan.net>
2021-10-26 15:08:04 -0500

    updating the svelte app to the latest version of  idvc

--- src/App.svelte
+++ src/App.svelte
@@ -3,58 +3,104 @@
 	import { onMount } from 'svelte';
 	import IDVC from '@idscan/idvc';
 
-	let publicKey = 'REPLACE ME WITH YOUR PUBLIC KEY';
-    	let backendServerUrl = 'REPLACE ME WITH YOUR BACKEND SERVICE URL';
-    	let licenseKey = 'REPLACE ME WITH YOUR LICENSE KEY';
-	
-	onMount( () => {
-			new IDVC({
-			el: 'videoCapturingEl',
-			networkUrl: '/assets/networks',
-			tapBackSide : true,
-			licenseKey: licenseKey,
+		
+	onMount( () => {		
+		let idvc = new IDVC({
+			el: "videoCapturingEl",
+			licenseKey: "LICENSE KEY REPLACE ME",
+			isShowManualSwitchButton: true,
+			showSubmitBtn: true,
+			isShowVersion: true,
+			tapOnVideo: false,
+			tapBackSide: false,
+			minPDFframes: 1,
+			parseMRZ: false,
+			tapFace: false,
+			enableLimitation: true,
+			autoContinue: true,
+			resizeUploadedImage: 1500,
+			showForceCapturingBtn: false,
+			fixFrontOrientAfterUpload: false,
+			enableFlash: false,
+			capturingMode: '4',
 			steps: [
-				{type: 'front', name: 'Front Scan'},
-				{type: 'face', name: 'Selfie'}
+				{ type: "front", name: "Front Scan" },
+				{ type: "face", name: "Selfie" },
 			],
-			submit (data) {
-				let backStep = data.steps.find(item => item.type === 'back')
-				let trackString = (backStep && backStep.trackString) ? backStep.trackString : ''
+			useCDN: true,
+			networkUrl: "/assets/networks",
+			showPreviewForOneStep: true,
+			priority: "auto",
+			realFaceMode: "all",
+			types: ["ID"],
+			strictAllowedTypes: false,
+			enableGeolocation: false,
+			displayParsedData: false,
+			onChange(data) {
+				console.log(data);
+			},
+			onCameraError(data) {
+				console.log(data);
+			},
+			onReset(data) {
+				console.log(data);
+			},
+			onRetakeHook(data) {
+				console.log(data);
+			},
+			submit(data) {
+				let backStep = data.steps.find((item) => item.type === "back");
+				let trackString =
+				backStep && backStep.trackString ? backStep.trackString : "";
 
 				let request = {
-					frontImageBase64: data.steps.find (item => item.type === 'front').img.split (/:image\/(jpeg|png);base64,/)[2],
-					backOrSecondImageBase64: backStep.img.split (/:image\/(jpeg|png);base64,/)[2],
-					faceImageBase64: data.steps.find (item => item.type === 'face').img.split (/:image\/(jpeg|png);base64,/)[2],
-					documentType: data.documentType,
-					trackString: trackString
-				}
-				
-				fetch ('https://dvs2.idware.net/api/Request', {
-					method: 'POST',
-					headers: {
-						'Content-Type': 'application/json;charset=utf-8',
-						'Authorization': `Bearer ${publicKey}`
-					},
-					body: JSON.stringify (request)
-				}).then (response => response.json ())
-					.then (response => {
-						fetch (backendServerUrl, {
-							method: 'POST',
-							headers: {
-								'Content-Type': 'application/json;charset=utf-8'
-							},
-							body: JSON.stringify ({
-								requestId: response.requestId
-							})
-						}).then (response => response.json ())
-							.then (data => {
-								console.log(data);
-							})
-					}).catch(() => {
-					
+				frontImageBase64: data.steps
+					.find((item) => item.type === "front")
+					.img.split(/:image\/(jpeg|png);base64,/)[2],
+				backOrSecondImageBase64: backStep.img.split(
+					/:image\/(jpeg|png);base64,/
+				)[2],
+				faceImageBase64: data.steps
+					.find((item) => item.type === "face")
+					.img.split(/:image\/(jpeg|png);base64,/)[2],
+				documentType: data.documentType,
+				trackString: trackString,
+				userAgent: window.navigator.userAgent,
+				captureMethod: data.captureMethod,
+				verifyFace: true,
+				};
+
+				fetch("https://dvs2.idware.net/api/Request", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json;charset=utf-8",
+					Authorization: "BEARER TOKEN REPLACE ME",
+				},
+				body: JSON.stringify(request),
 				})
-			}
-			});
+				.then((response) => response.json())
+				.then((response) => {
+					fetch("BACKEND SERVER URL REPLACE ME",
+					{
+						method: "POST",
+						headers: {
+						"Content-Type": "application/json;charset=utf-8",
+						},
+						body: JSON.stringify({
+						requestId: response.requestId,
+						}),
+					}
+					)
+					.then((response) => response.json())
+					.then((data) => {
+						console.log(data);
+					});
+				})
+				.catch((err) => {
+					console.err(err);
+				});
+			},
+		});
 	});
 </script>
 
@@ -63,24 +109,3 @@
 	<div id="videoCapturingEl"></div>
 </main>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>

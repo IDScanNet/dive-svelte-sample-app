4c56e893e0a822dd8efe85516662d3ffdae78103
IDSCAN\bmulhall <bmulhall@idscan.net>
2022-04-29 17:53:59 -0400

    updating the app

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,8 @@
 import svelte from 'rollup-plugin-svelte';
-import resolve from '@rollup/plugin-node-resolve';
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
-import css from 'rollup-plugin-css-only'
-import copy from 'rollup-plugin-copy'
+import css from "rollup-plugin-css-only";
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -30,63 +28,36 @@ function serve() {
 }
 
 export default {
-	external: ['@idscan/idvc'],
-	globals: {
-	  '@idscan/idvc': 'IDVC'
-	},
-	input: 'src/main.js',
-	output: {
-		sourcemap: true,
-		format: 'iife',
-		name: 'app',
-		file: 'public/build/bundle.js'
-	},
-	plugins: [
-		copy({
-			targets: [
-			  { src: 'node_modules/@idscan/idvc/dist/networks/keras/*', dest: 'public/assets/networks/keras' },
-			  { src: 'node_modules/@idscan/idvc/dist/networks/mrz/*', dest: 'public/assets/networks/mrz' },
-			  { src: 'node_modules/@idscan/idvc/dist/networks/types/*', dest: 'public/assets/networks/types' }
-			],
-			verbose: true
-		  }),
-		css({ output: "public/build/idvc.css" }),
-		svelte({
-			// enable run-time checks when not in production
-			dev: !production,
-			// we'll extract any component CSS out into
-			// a separate file - better for performance
-			css: css => {
-				css.write('bundle.css');
-			}
-		}),
+  input: "src/main.js",
+  output: {
+    format: "iife",
+    name: "app",
+    file: "public/build/bundle.js",
+  },
+  plugins: [
+    css({ output: "bundle.css" }),
+    svelte(),
 
-		// If you have external dependencies installed from
-		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration -
-		// consult the documentation for details:
-		// https://github.com/rollup/plugins/tree/master/packages/commonjs
-		resolve({
-			browser: true,
-			dedupe: ['svelte']
-		}),
-		commonjs({
-			include: 'node_modules/**'
-		}),
+    // If you have external dependencies installed from
+    // npm, you'll most likely need these plugins. In
+    // some cases you'll need additional configuration -
+    // consult the documentation for details:
+    // https://github.com/rollup/plugins/tree/master/packages/commonjs
+    nodeResolve(),
+    commonjs(),
+    // In dev mode, call `npm run start` once
+    // the bundle has been generated
+    !production && serve(),
 
-		// In dev mode, call `npm run start` once
-		// the bundle has been generated
-		!production && serve(),
+    // Watch the `public` directory and refresh the
+    // browser on changes when not in production
+    !production && livereload("public"),
 
-		// Watch the `public` directory and refresh the
-		// browser on changes when not in production
-		!production && livereload('public'),
-
-		// If we're building for production (npm run build
-		// instead of npm run dev), minify
-		production && terser()
-	],
-	watch: {
-		clearScreen: false
-	}
+    // If we're building for production (npm run build
+    // instead of npm run dev), minify
+    //production && terser(),
+  ],
+  watch: {
+    clearScreen: false,
+  },
 };
--- src/App.svelte
+++ src/App.svelte
@@ -5,102 +5,134 @@
 
 		
 	onMount( () => {		
-		let idvc = new IDVC({
-			el: "videoCapturingEl",
-			licenseKey: "LICENSE KEY REPLACE ME",
-			isShowManualSwitchButton: true,
-			showSubmitBtn: true,
-			isShowVersion: true,
-			tapOnVideo: false,
-			tapBackSide: false,
-			minPDFframes: 1,
-			parseMRZ: false,
-			tapFace: false,
-			enableLimitation: true,
-			autoContinue: true,
-			resizeUploadedImage: 1500,
-			showForceCapturingBtn: false,
-			fixFrontOrientAfterUpload: false,
-			enableFlash: false,
-			capturingMode: '4',
-			steps: [
-				{ type: "front", name: "Front Scan" },
-				{ type: "face", name: "Selfie" },
-			],
-			useCDN: true,
-			networkUrl: "/assets/networks",
-			showPreviewForOneStep: true,
-			priority: "auto",
-			realFaceMode: "all",
-			types: ["ID"],
-			strictAllowedTypes: false,
-			enableGeolocation: false,
-			displayParsedData: false,
-			onChange(data) {
-				console.log(data);
-			},
-			onCameraError(data) {
-				console.log(data);
-			},
-			onReset(data) {
-				console.log(data);
-			},
-			onRetakeHook(data) {
-				console.log(data);
-			},
-			submit(data) {
-				let backStep = data.steps.find((item) => item.type === "back");
-				let trackString =
-				backStep && backStep.trackString ? backStep.trackString : "";
+ 		new IDVC({
+            el: 'videoCapturingEl',
+            licenseKey: '',
+            isShowManualSwitchButton: true,
+            showSubmitBtn: true,
+            isShowVersion: true,
+            tapOnVideo: false,
+            tapBackSide: false,
+            minPDFframes: 1,
+            parseMRZ: false,
+            tapFace: false,
+            enableLimitation: false,
+            autoContinue: true,
+            resizeUploadedImage: 1500,
+            showForceCapturingBtn: false,
+            fixFrontOrientAfterUpload: false,
+            enableFlash: false,
+            capturingMode: '4',
+            steps: [
+                { type: 'front', name: 'Front Scan' },
+                { type: 'face', name: 'Selfie' },
+            ],
+            useCDN: true,
+            networkUrl: '/assets/networks',
+            showPreviewForOneStep: true,
+            priority: 'auto',
+            realFaceMode: 'all',
+            types: ['ID', 'Passport', 'PassportCard', 'GreenCard', 'InternationalId' ],
+            strictAllowedTypes: false,
+            enableGeolocation: false,
+            displayParsedData: false,
+            onChange(data) {
+                console.log(data);
+            },
+            onCameraError(data) {
+                console.log(data);
+            },
+            onReset(data) {
+                 console.log(data);
+            },
+            onRetakeHook(data) {
+                console.log(data);
+            },
+            submit(data) {   
+                let frontImage,backImage,faceImage;
+                let trackString;
+                let frontStep, backStep, faceStep;
 
-				let request = {
-				frontImageBase64: data.steps
-					.find((item) => item.type === "front")
-					.img.split(/:image\/(jpeg|png);base64,/)[2],
-				backOrSecondImageBase64: backStep.img.split(
-					/:image\/(jpeg|png);base64,/
-				)[2],
-				faceImageBase64: data.steps
-					.find((item) => item.type === "face")
-					.img.split(/:image\/(jpeg|png);base64,/)[2],
-				documentType: data.documentType,
-				trackString: trackString,
-				userAgent: window.navigator.userAgent,
-				captureMethod: data.captureMethod,
-				verifyFace: true,
-				};
+                switch(data.documentType){
+                // Drivers License and Identification Card
+                case 1:
+                        frontImage = data.steps
+                        .find((item) => item.type === "front")
+                        .img.split(/:image\/(jpeg|png);base64,/)[2];
 
-				fetch("https://dvs2.idware.net/api/Request", {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json;charset=utf-8",
-					Authorization: "BEARER TOKEN REPLACE ME",
-				},
-				body: JSON.stringify(request),
-				})
-				.then((response) => response.json())
-				.then((response) => {
-					fetch("BACKEND SERVER URL REPLACE ME",
-					{
-						method: "POST",
-						headers: {
-						"Content-Type": "application/json;charset=utf-8",
-						},
-						body: JSON.stringify({
-						requestId: response.requestId,
-						}),
-					}
-					)
-					.then((response) => response.json())
-					.then((data) => {
-						console.log(data);
-					});
-				})
-				.catch((err) => {
-					console.err(err);
-				});
-			},
-		});
+
+                        backStep = data.steps.find((item) => item.type === "back");
+                        trackString = backStep && backStep.trackString ? backStep.trackString : "";
+                        backImage = backStep.img.split(/:image\/(jpeg|png);base64,/)[2];
+
+
+                        faceImage = data.steps
+                        .find((item) => item.type === "face")
+                        .img.split(/:image\/(jpeg|png);base64,/)[2];
+
+                        break;
+                    // US and International Passports
+                    case 2:
+                        frontStep = data.steps.find((item) => item.type === "front");
+                        trackString = frontStep && frontStep.mrzText ? frontStep.mrzText : "";
+                        frontImage = frontStep.img.split(/:image\/(jpeg|png);base64,/)[2];
+
+                        faceImage = data.steps
+                        .find((item) => item.type === "face")
+                        .img.split(/:image\/(jpeg|png);base64,/)[2];
+                    
+                        break;
+                    // US Passport Cards,US Green Cards,International IDs with 3 line MRZs  
+                    case 3:
+                    case 6:
+                    case 7:
+                        frontImage = data.steps
+                            .find((item) => item.type === "front")
+                            .img.split(/:image\/(jpeg|png);base64,/)[2];
+
+                        backStep = data.steps.find((item) => item.type === "back");
+                        trackString = backStep && backStep.mrzText ? backStep.mrzText : "";
+                        backImage = backStep.img.split(/:image\/(jpeg|png);base64,/)[2];
+
+
+                        faceImage = data.steps
+                            .find((item) => item.type === "face")
+                            .img.split(/:image\/(jpeg|png);base64,/)[2];
+                        break;
+                default:
+
+                }
+
+                let request = {
+                    frontImageBase64: frontImage,
+                    backOrSecondImageBase64: backImage,
+                    faceImageBase64: faceImage,
+                    documentType: data.documentType,
+                    trackString: trackString,
+                    userAgent: window.navigator.userAgent,
+                    captureMethod: data.captureMethod,
+                    verifyFace: true,
+                };
+
+                fetch(
+                    "https://us-central1-idscan-backend.cloudfunctions.net/api/Verify",
+                    {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json;charset=utf-8",
+                    },
+                    body: JSON.stringify(request),
+                    }
+                )
+                .then((response) => response.json())
+                .then((data) => {
+                        console.log(data);
+                })
+                .catch((err) => {
+                    console.err(err);
+                });
+            },
+        });
 	});
 </script>
 
